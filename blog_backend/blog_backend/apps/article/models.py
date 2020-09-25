145bb71e5922363f9358d1d0d3a3c776924757d9
bygzyz <zhenghuanluck@163.com>
2020-09-25 16:31:31 +0800

    feat:模型类 article_list增加字段image,并指定上传图片的本地地址

--- blog_backend/blog_backend/apps/article/models.py
+++ blog_backend/blog_backend/apps/article/models.py
@@ -19,13 +19,18 @@ class ArticleList(BaseModel):
     summary = models.CharField(max_length=200,
                                null=True,
                                verbose_name="文章摘要")
+    category = models.CharField(max_length=100,
+                                null=False,
+                                default="python",
+                                verbose_name="所属类别")
     author = models.CharField(max_length=50,
                               null=False,
                               verbose_name="作者")
+    image = models.ImageField(upload_to='article_list', verbose_name='文章图片', null=True)
 
     class Meta:
         db_table = 'article_list'
-        verbose_name = '商品SKU'
+        verbose_name = '文章列表'
         verbose_name_plural = verbose_name
 
 
@@ -43,15 +48,20 @@ class ArticleDetail(BaseModel):
     summary = models.CharField(max_length=200,
                                null=True,
                                verbose_name="文章摘要")
+    category = models.CharField(max_length=100,
+                                null=False,
+                                default="python",
+                                verbose_name="所属类别")
     author = models.CharField(max_length=50,
                               null=False,
                               verbose_name="作者")
     content = models.TextField(verbose_name="文章详情内容")
+    # image = models.ImageField(upload_to='article_detail', verbose_name='文章图片', null=True)
     image_url = models.CharField(max_length=500,
                                  null=True,
                                  verbose_name="图片链接")
 
     class Meta:
         db_table = 'article_detail'
-        verbose_name = '商品SKU'
+        verbose_name = '文章详情'
         verbose_name_plural = verbose_name
