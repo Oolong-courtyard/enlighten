88549545210b21c19b5ae0322f6cf194ebbb2020
bygzyz <zhenghuanluck@163.com>
2020-10-30 17:18:56 +0800

    feat:配置新视图(UsernameCountView)的url

--- blog_backend/blog_backend/apps/users/urls.py
+++ blog_backend/blog_backend/apps/users/urls.py
@@ -7,6 +7,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url('^usernames-count',views.UsernameCountView.as_view()),
     url('^register$', views.RegisterView.as_view()),
     url('^login$', views.LoginView.as_view()),
+
 ]
