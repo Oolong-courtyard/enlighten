05f0fa0c4a822a7fc8b04fc6c1e3ec56ad3068b5
bygzyz <zhenghuanluck@163.com>
2020-11-10 17:34:13 +0800

    feat:新建应用用于第三方登录

---
+++ blog_backend/blog_backend/apps/oauth/models.py
@@ -0,0 +1,20 @@
+"""
+第三方登录用户模型类
+"""
+from django.db import models
+
+from blog_backend.utils.models import BaseModel
+
+
+class OAuthQQUser(BaseModel):
+    """
+    保存QQ登录用户数据
+    """
+    user = models.ForeignKey('users.UserProfile', on_delete=models.CASCADE, verbose_name='用户')
+    # db_index = True 设置数据库索引
+    openid = models.CharField(max_length=64, verbose_name='openid', db_index=True)
+
+    class Meta:
+        db_table = 'oauth_qq'
+        verbose_name = 'QQ登录用户数据'
+        verbose_name_plural = verbose_name
