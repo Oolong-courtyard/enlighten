2667d72ebc5894e415c74d8d55432836a187ba42
bygzyz <zhenghuanluck@163.com>
2020-12-17 18:09:42 +0800

    feat:响应数据格式统一

--- blog_backend/blog_backend/utils/base_response.py
+++ blog_backend/blog_backend/utils/base_response.py
@@ -1,5 +1,6 @@
 """
-所有API接口返回标准化
+所有API接口响应数据标准化;
+业务响应状态码;
 """
 from rest_framework.response import Response
 from rest_framework import status
@@ -21,6 +22,7 @@ class BaseResponse(Response):
                  exception=False,
                  content_type="application/json"):
         super().__init__(status=status)
+        self.status = status
         self.data = {"code": code, "message": message, "data": data}
         self.template_name = template_name
         self.headers = headers
@@ -30,3 +32,24 @@ class BaseResponse(Response):
         if headers:
             for name, value in headers.items():
                 self[name] = value
+
+
+class BusStatusCode:
+    """
+    业务响应状态码;
+    后续根据需要添加业务状态码
+    """
+
+    # 正常
+    OK_1001 = {"code": 1001, "message": "响应正常"}
+
+    # 客户端异常
+    BAD_REQUEST_4001 = {"code": 4001, "message": "两次密码不一致"}
+    BAD_REQUEST_4002 = {"code": 4002, "message": "手机号格式错误"}
+    BAD_REQUEST_4004 = {"code": 4004, "message": "数据对象不存在"}
+    BAD_REQUEST_4007 = {"code": 4006, "message": "密码校验失败"}
+    BAD_REQUEST_4006 = {"code": 4007, "message": "密码格式错误"}
+
+    # 服务端异常
+    INTERNAL_SERVER_ERROR_5001 = {"code": 5001, "message": "数据库错误"}
+    INTERNAL_SERVER_ERROR_5002 = {"code": 5002, "message": "服务器错误"}
--- blog_backend/blog_backend/utils/exceptions.py
+++ blog_backend/blog_backend/utils/exceptions.py
@@ -1,13 +1,17 @@
 """
-配置异常处理信息，增加数据库异常处理和redis异常处理
+自定义异常处理信息,保证响应格式一致性;
+增加数据库异常处理和redis异常处理等
 """
 import logging
+import traceback
 
+from django.core.exceptions import ObjectDoesNotExist
 from django.db import DatabaseError
 from redis.exceptions import RedisError
 from rest_framework.views import exception_handler as drf_exception_handler
-from rest_framework.response import Response
 from rest_framework import status
+
+from utils.base_response import BaseResponse, BusStatusCode
 
 # 获取在配置文件中定义的logger，用来记录日志
 logger = logging.getLogger('django')
@@ -22,16 +26,28 @@ def exception_handler(exc, context):
     """
     # 调用drf框架原生的异常处理方法
     response = drf_exception_handler(exc, context)
+    # 控制台打印出报错的栈信息
+    traceback.print_exc()
 
     if response is None:
         view = context['view']
         if isinstance(exc, DatabaseError) or isinstance(exc, RedisError):
             # 数据库异常
             logger.error('[%s] %s' % (view, exc))
-            response = Response({'message': '服务器内部错误'},
-                                status=status.HTTP_507_INSUFFICIENT_STORAGE)
+            response = BaseResponse(**BusStatusCode.INTERNAL_SERVER_ERROR_5001,
+                                    status=status.HTTP_507_INSUFFICIENT_STORAGE)
+        elif isinstance(exc, ObjectDoesNotExist):
+            # 数据查询不到异常
+            logger.error('[%s] %s' % (view, exc))
+            response = BaseResponse(**BusStatusCode.BAD_REQUEST_4004,
+                                    status=status.HTTP_404_NOT_FOUND)
+
         else:
             logger.error('[%s] %s' % (view, exc))
+            response = BaseResponse(code=65535,
+                                    message="服务器异常",
+                                    data="",
+                                    status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     else:
         logger.error(' %s' % (exc,))
 
