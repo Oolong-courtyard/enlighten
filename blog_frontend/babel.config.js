1e7d473fa217d887f715edd7e58446673689c58b
bygzyz <zhenghuanluck@163.com>
2021-01-14 22:44:58 +0800

    feat:点击发布文章按钮,将内容的html发送给后台入库

--- blog_frontend/babel.config.js
+++ blog_frontend/babel.config.js
@@ -1,5 +1,14 @@
+//这是项目发布阶段用到的 babel 插件
+const prodPlugins = []
+if (process.env.NODE_ENV === 'production') {
+  prodPlugins.push('transform-remove-console')
+}
+
 module.exports = {
   presets: [
     '@vue/app'
+  ],
+  plugins: [
+    // ...prodPlugins,
   ]
 }
