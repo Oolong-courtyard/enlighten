0fa1f30ea9b12ce887755fda98f74ee911fa2802
liuzhenghuan <liuzhenghuan@datagrand.com>
2021-06-25 19:13:11 +0800

    feat：疏通权限认证接口

--- blog_backend_go/pkg/app/request.go
+++ blog_backend_go/pkg/app/request.go
@@ -1,1 +1,14 @@
 package app
+
+import (
+	"fmt"
+	"github.com/astaxie/beego/validation"
+)
+
+//MarkErrors logs error logs
+func MarkErrors(errors []*validation.Error) {
+	for _, err := range errors {
+		fmt.Println(err.Key, err.Message)
+		//logging.Info(err.Key, err.Message)
+	}
+}
--- blog_backend_go/routers/api/auth.go
+++ blog_backend_go/routers/api/auth.go
@@ -2,8 +2,11 @@ package api
 
 import (
 	"blog_backend_go/pkg/app"
+	"blog_backend_go/pkg/e"
+	"blog_backend_go/service/auth_service"
 	"github.com/astaxie/beego/validation"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 type auth struct {
@@ -23,7 +26,20 @@ func GetAuth(c *gin.Context) {
 
 	if !ok {
 		app.MarkErrors(valid.Errors)
-
+		appG.Response(http.StatusBadRequest, e.INVALID_PARAMS, nil)
+		return
 	}
+
+	authService := auth_service.Auth{Username: username, Password: password}
+	isExist, err := authService.Check()
+	if err != nil {
+		appG.Response(http.StatusInternalServerError, e.ERROR_AUTH_CHECK_TOKEN_FAIL, nil)
+	}
+
+	if !isExist {
+		appG.Response(http.StatusInternalServerError, e.ERROR_AUTH_TOKEN, nil)
+	}
+
+
 
 }
