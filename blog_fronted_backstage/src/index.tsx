6dc60dc7c92b60d7c971e8f09161313927e19867
liuzhenghuan <liuzhenghuan@datagrand.com>
2021-07-09 14:03:50 +0800

    feat: 使用脚手架工具 npx 重新搭建react项目

--- blog_fronted_backstage/src/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import "react-app-polyfill/ie11"
-import "react-app-polyfill/stable"
-import Router from "./router/router"
-
-ReactDOM.render(<Router />, document.getElementById("root"))
